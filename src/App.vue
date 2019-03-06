3582864ac583b97944b9c6b8c2f1fa58fc8ecee8
Francesca Citter <francesca.citter@link-me.it>
2019-03-06 19:58:01 +0100

    shadow created

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="wrapper">
+    <bouncing-image imgurl="http://cdn.onlinewebfonts.com/svg/img_358086.png"/>
+  </div>
+  <!-- <toggle imgurl="http://cdn.onlinewebfonts.com/svg/img_358086.png"/> -->
+</template>
+
+<script>
+import BouncingImage from './components/BoucingImage.vue'
+
+export default {
+  name: 'app',
+  components: {
+    BouncingImage,
+  }
+}
+</script>
+
+<style>
+
+.wrapper {
+  padding: 50px;
+}
+
+</style>
---
+++ src/components/BoucingImage.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="ball-wrapper">
+         <img :src="imgurl" class="bounce"> 
+  <div class="shadow"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BoucingImage',
+  props: {
+    imgurl: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  img{
+    max-width: 50px;
+    display: block;
+  }
+  .ball-wrapper{
+    display:flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+
+  .shadow{
+    width: 50px;
+    background-color: black;
+    height: 3px;
+    animation: enlarge 1s infinite alternate;
+  }
+
+@keyframes enlarge{
+    from {
+    width:50px;
+    background-color: black;
+    opacity: 1;
+    height: 3px;
+    border-radius: 35%;
+  }
+  to {
+    width:80px;
+    background-color: grey;
+    opacity: .5;
+    height: 8px;
+    border-radius: 50%;
+  }
+}
+
+
+@keyframes bounce {
+  from {
+    transform: translateY(0px);
+  }
+  to {
+    transform: translateY(-15px);
+  }
+}
+
+  .bounce { 
+  animation: bounce 1s infinite alternate;
+   
+}
+
+
+</style>
